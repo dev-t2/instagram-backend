35629862a6debaeedd8777970337b3d42acda910
t2 <oo_ri@naver.com>
2021-06-07 01:52:53 +0900

    Apollo Server Setup

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # InstaClone Backend
+
+- Node: https://nodejs.org/ko/
+- Apollo Server: https://www.apollographql.com/docs/apollo-server/
---
+++ app.js
@@ -0,0 +1,19 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server
+  .listen()
+  .then(() => console.log('Server is running on http://localhost:4000'));
