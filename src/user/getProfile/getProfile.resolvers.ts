1bcdce9dfe0639ba9ba82792561186d09078a85c
t2 <oo_ri@naver.com>
2021-06-13 18:47:49 +0900

    Get Profile Include Follower and Following Update

--- src/user/getProfile/getProfile.resolvers.ts
+++ src/user/getProfile/getProfile.resolvers.ts
@@ -5,6 +5,7 @@ const resolvers: Resolvers = {
     getProfile: async (_, { nickname }, { client }) => {
       const user = await client.user.findUnique({
         where: { nickname },
+        include: { follower: true, following: true },
       });
 
       return user;
