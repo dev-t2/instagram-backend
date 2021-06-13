7d9e4c4d66ade60dcd349a23b4c6ce9ce7d9f037
t2 <oo_ri@naver.com>
2021-06-13 18:46:47 +0900

    Search User Include Follower and Following Update

--- src/user/searchUser/searchUser.resolvers.ts
+++ src/user/searchUser/searchUser.resolvers.ts
@@ -5,6 +5,7 @@ const resolvers: Resolvers = {
     searchUsers: async (_, { keyword }, { client }) => {
       const users = await client.user.findMany({
         where: { nickname: { contains: keyword } },
+        include: { follower: true, following: true },
       });
 
       return users;
