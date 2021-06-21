6d9466008deaec460933e453a010108f59c5e8ef
t2 <oo_ri@naver.com>
2021-06-21 20:35:12 +0900

    Login Error Message Update

--- src/user/login/login.resolvers.ts
+++ src/user/login/login.resolvers.ts
@@ -15,22 +15,29 @@ const resolvers: Resolvers = {
         if (!user) {
           return {
             isSuccess: false,
-            error:
-              '입력한 닉네임을 사용하는 계정을 찾을 수 없습니다. 닉네임을 확인하고 다시 시도하세요.',
+            error: '입력된 닉네임이 올바르지 않습니다.',
           };
         }
 
         const isPassword = await bcrypt.compare(password, user.password);
 
         if (!isPassword) {
-          return { isSuccess: false, error: 'Invalid Password' };
+          return {
+            isSuccess: false,
+            error: '입력된 비밀번호가 올바르지 않습니다.',
+          };
         }
 
         const token = await jwt.sign({ id: user.id }, process.env.SECRET_KEY);
 
         return { isSuccess: true, token };
       } catch (error) {
-        return { isSuccess: false, error };
+        console.error(error);
+
+        return {
+          isSuccess: false,
+          error: '서버 에러가 발생했습니다. 관리자에게 문의하세요.',
+        };
       }
     },
   },
