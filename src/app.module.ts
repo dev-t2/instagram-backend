84d38528685a8da6b98837b2036455d97d728b33
dev-t2 <oo_ri@naver.com>
2022-11-09 22:05:58 +0900

    Configuration and Joi Install and Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 
-@Module({})
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('development', 'production').default('development'),
+      }),
+      validationOptions: { abortEarly: true },
+    }),
+  ],
+})
 export class AppModule {}
