7d76cb7f423f2d65541c842d9fa1d45a9f6db38a
dev-t2 <oo_ri@naver.com>
2022-11-09 21:52:18 +0900

    HTTP Exception Filter Update

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
-import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { HttpExceptionFilter } from './common/filters';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
